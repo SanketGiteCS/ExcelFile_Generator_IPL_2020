733e3bd7b36ed5a4e81f0e53daf2a80ac626bc64
SanketGiteCS <sanketgite1330@gmail.com>
2021-12-07 21:16:51 +0530

    Done IPL_2020_Scrapper

--- CricinfoExcel.js
+++ CricinfoExcel.js
@@ -2,11 +2,55 @@ const request = require("request");
 const cheerio = require("cheerio");
 const chalk = require('chalk');
 const fs = require("fs");
-const ExcelJS = require('exceljs');
-const workbook = new ExcelJS.Workbook();
-workbook.creator = 'Me';
-workbook.lastModifiedBy = 'Her';
-workbook.created = new Date(1985, 8, 30);
-workbook.modified = new Date();
-workbook.lastPrinted = new Date(2016, 9, 27);
-const sheet = workbook.addWorksheet('My Sheet');
+const xlsx = require('xlsx');
+
+const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results";
+request(url,callback);
+
+let matches = []; 
+function callback(err,res,html){
+    const $ = cheerio.load(html);
+    let matchesObj = $("a[data-hover='Scorecard']");
+    // for(let i = 0;i<1;i++){
+        let ScoreBoardUrl = "https://www.cricinfo.com"+$(matchesObj[0]).attr("href");
+        matches.push({
+            "MatcheUrl" : ScoreBoardUrl,
+            "Innings" :[]
+        })
+        request(ScoreBoardUrl,inningFinder.bind(this,0)); 
+    // }
+}
+
+function getBattingData(BattingData){
+    const SelectorTool = cheerio.load(BattingData);
+    let thead = SelectorTool("");
+    let tbody = SelectorTool("");
+    console.log(tbody.length);
+    let tfooter = SelectorTool("");
+
+}
+function getBowlingData(BowlingData){
+    const SelectorTool = cheerio.load(BowlingData);
+
+}
+
+function inningFinder(index,err,res,html){
+    const $ = cheerio.load(html);
+    let BattingInfoObj = $(".Collapsible__contentInner .table.batsman").html();
+    //let BowlingInfoObj = $(".Collapsible__contentInner .table.bowler");
+    fs.writeFileSync("temp.html",BattingInfoObj);
+    getBattingData(BattingInfoObj);
+    
+
+    // let BattingInning1 = $(BattingInfoObj).html();
+    // getBattingData(BattingInning1); 
+
+    // let BowlingInning1 = $(BowlingInfoObj[0]).html();
+    // getBowlingData(BowlingInning1);
+
+    // let BattingInning2 = $(BattingInfoObj[1]).html();
+    // getBattingData(BattingInning2);
+
+    // let BowlingInning2 = $(BowlingInfoObj[1]).html();
+    // getBowlingData(BowlingInning2);
+}
